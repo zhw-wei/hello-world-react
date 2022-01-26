cbd8a917b4a7ebf0aac48242b452d6f7c7122da4
zhw-wei <2261842924@qq.com>
2022-01-26 22:48:17 +0800

    新增scss

--- src/comp3/About/About.tsx
+++ src/comp3/About/About.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
+//必须要使用module.css后缀，否则无法import引入
+import style from './About.module.scss'
+
 export default class About extends Component {
     render() {
         return (
-            <div>
+            <div className={style.copm3_about}>
                 
                 <h2>hello world about</h2>
             </div>
